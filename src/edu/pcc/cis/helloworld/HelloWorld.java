3cc531e66c39f235e44ab464a21730eb83577e10
javanity <wisdom2freedom@gmail.com>
2017-02-06 11:58:34 -0800

    Initialize project

---
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu.pcc.cis.helloworld;
+
+/**
+ * Simple class to practice IntelliJ, Git and Github
+ *
+ * @author  Valerie Baker
+ * @version 02/06/2017
+ *
+ */
+public class HelloWorld
+{
+}
+
+
